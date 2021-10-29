40079df37b0d5591cdae9fdd8e1784ec296f1709
Dimert010 <89783123+Dimert010@users.noreply.github.com>
2021-10-29 09:13:10 +0200

    Delete week.py

--- week.py
+++
@@ -1,7 +0,0 @@
-welkedag = input('geef een dag van de week: ')
-alledagen = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag']
-maakIndex = alledagen.index(welkedag)
-sliceIndex = alledagen[:maakIndex+1]
-while welkedag in alledagen:
-    print(*sliceIndex)
-    break
