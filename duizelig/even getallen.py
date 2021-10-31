58c7a9a9e0e9ec2b46b0d42d100167959a8645a8
Dimert010 <89783123+Dimert010@users.noreply.github.com>
2021-10-31 22:17:33 +0100

    Add files via upload

---
+++ duizelig/even getallen.py
@@ -0,0 +1,5 @@
+i = 22
+
+while i <= 50:
+    print(i)
+    i=i+2
---
+++ duizelig/time.py
@@ -0,0 +1,9 @@
+i = 24
+x = 11
+
+while i >= 12:
+    print(str(i) +' PM')
+    i=i-1
+while x >= 00:
+    print(str(x) +' AM')
+    x=x-1
